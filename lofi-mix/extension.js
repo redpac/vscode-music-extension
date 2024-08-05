76b9222cf08bbcf963049d79769875a0373d930b
chinmay jayant khamkar <khamkarchinmay4@gmail.com>
2024-08-04 22:27:01 -0700

    hmmm

--- lofi-mix/extension.js
+++ lofi-mix/extension.js
@@ -13,7 +13,7 @@ function activate(context) {
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "lofi-mix" is now active!');
+	console.log('Congratulations, your extension "lofi-mix" is now active!!');
 
 	const lofiMixViewProvider = new LofiMixViewProvider(context.extensionUri)
 
